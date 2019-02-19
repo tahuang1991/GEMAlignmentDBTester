09c26f8e4b743950d484ed3afb65cb5c02fa58ef
Ian J. Watson <ian.james.watson@cern.ch>
2019-02-19 14:19:34 +0900

    Add config to test GEM builder

---
+++ MyGEMRcdMaker/test/TestGEMBuild.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MyGEMAlignmentRcdRetrieveTest")
+process.load("Geometry.GEMGeometryBuilder.gemGeometryDB_cfi")
+process.GEMGeometryESModule.applyAlignment = cms.bool(True)
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
+
+process.GlobalTag.toGet = cms.VPSet(
+    cms.PSet(
+        record = cms.string('GEMAlignmentRcd'),
+        tag = cms.string("GEMAlignment_test"),
+        connect = cms.string("sqlite:./MyAlignment.db"),
+    ),
+    # cms.PSet(
+    #     record = cms.string('GEMAlignmentErrorRcd'),
+    #     tag = cms.string("myGEMAlignment_test"),
+    #     connect = cms.string("sqlite:./MyAlignment.db"),
+    # ),
+    cms.PSet(
+        record = cms.string('GEMAlignmentErrorExtendedRcd'),
+        tag = cms.string("GEMAlignmentErrorExtended_test"),
+        connect = cms.string("sqlite:./MyAlignment.db"),
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+process.source = cms.Source("EmptySource")
+
+process.test = cms.EDAnalyzer("GEMTestGeometryAnalyzer")
+
+process.path = cms.Path(process.test)
